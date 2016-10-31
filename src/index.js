a8e9e321913c536e8548484acbbbbe852017e2ef
Miguel Marques <mig.marques@gmail.com>
2016-10-31 20:40:43 +0000

    async version

--- src/index.js
+++ src/index.js
@@ -1,28 +1,65 @@
 'use strict';
 
 const jsdom = require('jsdom');
-let words = ['dog', 'cat', 'banana'];
-//words = ['tomato'];
+const async = require('async');
 
-let input = {en: 'dog', pt: 'cão', es: 'perro', de: 'Hund'};
+const Converter = require('csvtojson').Converter;
+const converter = new Converter({});
 
-const gbSelector = '*[data-src=hc_dict] .pron';
-const usSelector = '*[data-src=rHouse] .pron';
-const ptSelector = '';
-const deSelector = '';
-const esSelector = '';
+let pool = [];
 
-function getResults() {
-for (const word of words) {
-  jsdom.env(
-  `http://www.thefreedictionary.com/${word}`,
-  ['http://code.jquery.com/jquery.js'],
-  function(err, window) {
-    let en = window.$(gbSelector);
-    let us = window.$(usSelector);
-    console.log('Requesting', word, 'and the result in en_GB is:', en[0].innerHTML, 'and in en_US is', us[0].innerHTML);
+const config = {
+  enUS: {url: 'http://us.thefreedictionary.com/', selector: '*[data-src=rHouse] .pron'},
+  enGB: {url: 'http://en.thefreedictionary.com/', selector: '*[data-src=hc_dict] .pron'},
+  ptPT: {url: 'http://pt.thefreedictionary.com/', selector: '*[data-src=kdict] .pron'},
+  esES: {url: 'http://es.thefreedictionary.com/', selector: '*[data-src=kdict] .pron'},
+  deDE: {url: 'http://de.thefreedictionary.com/', selector: '*[data-src=kdict] .pron'},
+};
+
+converter.fromFile('test/input.csv', function(err, inputs) {
+  async.each(inputs,
+    function(input, callback) {
+      const cfg = config; return fetchResults(cfg, input, callback);
+     },
+  function(err) {
+    if(!err) {
+      console.log('complete');
+    } else {
+      console.log('Something has failed  with errors');
+    }
+  });
+});
+
+function fetchLanguageResult(languageConfig, input, callback) {
+  jsdom.env(`${languageConfig.url}${encodeURIComponent(input)}`,
+           ['http://code.jquery.com/jquery.js'],
+           function(err, window) {
+             let text = window.$(languageConfig.selector);
+             if(text.length > 0) {
+               console.log('Requesting', input,
+                           'and the result is:', text[0].innerHTML);
+                           pool.push(text[0].innerHTML);
+                            return callback();
+             }else {
+               console.log('Failed ->', input);
+               callback('Something went wrong');
+             }
+           });
+}
+
+function fetchResults(config, input, callback) {
+  let languages = Object.keys(input);
+
+  async.each(languages, function(language, callback) {
+    let languageConfig = config[language];
+    let word = input[language];
+    fetchLanguageResult(languageConfig, word, callback);
+  }, function(err) {
+    if(!err) {
+      console.log('complete');
+      console.log(pool);
+    } else {
+      console.log('Something has failed  with errors');
+    }
   });
 }
-}
-
-getResults();
